cdb7d7e04f35956e19751f0396007168ae2fa12b
Calebbii <biicaleb316@gmail.com>
2022-08-03 02:14:30 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name: "caleb",
+    streetAddress: "Kejani"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let updateList = {...employee};
+    updateList[key] = value;
+    return updateList;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    let clone = {...employee};
+    delete clone[key];
+    return clone;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
